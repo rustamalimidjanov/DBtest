bb807edb84a06c3a60695dbcb1b131062cf92137
rus <rustamalimidjanov@gmail.com>
2023-03-07 22:13:28 +0300

    fix vm

--- app/src/main/java/com/example/testest/FragmentMain.kt
+++ app/src/main/java/com/example/testest/FragmentMain.kt
@@ -4,20 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.arch.core.util.Function
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.*
 import androidx.lifecycle.Observer
 import com.example.testest.databinding.FragmentMainBinding
-import kotlinx.coroutines.launch
 import java.util.*
 
 class FragmentMain: Fragment(){
     private lateinit var binding: FragmentMainBinding
-    private lateinit var name: Name
     private val nameViewModel: FragmentViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -25,15 +19,13 @@ class FragmentMain: Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        name = Name()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.text = ""
-        nameViewModel.saveEditState(state = binding.editText.text.toString())
+        clearText()
 
         nameViewModel.getNamesLiveData.observe(
             viewLifecycleOwner,
@@ -42,6 +34,7 @@ class FragmentMain: Fragment(){
                     names.forEach { name ->
                         val text = "ID = ${name.id}, TEXT= ${name.name} \n"
                         binding.textView.append(text)
+                        binding.editText.setText(name.name)
                     }
                     updateUI()
                 }
@@ -55,6 +48,10 @@ class FragmentMain: Fragment(){
             val nameId: UUID = UUID.randomUUID()
             val name = Name(id = nameId, name = edText)
             nameViewModel.addName(name = name)
+
         }
+    }
+    private fun clearText() {
+        binding.textView.text = ""
     }
 }
--- app/src/main/java/com/example/testest/FragmentViewModel.kt
+++ app/src/main/java/com/example/testest/FragmentViewModel.kt
@@ -1,20 +1,12 @@
 package com.example.testest
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.util.*
 
 class FragmentViewModel: ViewModel() {
     private val nameRepository = NameRepository.get()
     val getNamesLiveData = nameRepository.getNames()
 
-    private val _setEditNameLiveData = MutableLiveData<String>()
-    val setEditNameLiveData: LiveData<String> = _setEditNameLiveData
-
-    fun saveEditState(state: String) {
-        _setEditNameLiveData.value = state
-    }
     fun getNames() {
         nameRepository.getNames()
     }
